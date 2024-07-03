6489e0c2e6c307988806f6e44a294ceab0aedae7
Simo0hi <154542003+Simo0hi@users.noreply.github.com>
2024-07-03 14:42:02 +0300

    Create Item.py

---
+++ Item.py
@@ -0,0 +1,11 @@
+class Item:
+    def __init__(self, name, price, restaurant):
+        self.restaurant = restaurant
+        self.name = name
+        self.price = price
+        self.calorie = calorie
+
+
+    def get_menu(self):
+        return self.items
+      
