e4eabc576b16529079deb42c209b388579d3ce8b
Alaa Fadhlallah <103293663+alaa-fa@users.noreply.github.com>
2024-07-03 14:49:29 +0300

    Update User.py
    - add docstring and cart item

--- User.py
+++ User.py
@@ -1,5 +1,22 @@
 class User:
-  
+    """ 
+    A class to represent a User.
+    ...
+    Attributes
+    ----------
+    name : string 
+    password : int 
+    phoneNumber : int 
+    email : str 
+    age : int 
+    signedIn : boolean
+    cart : Cart object
+
+    Methods
+    -------
+    signIn : makes signedIn = True in if password is correct, else prints false
+    signOut : makes signedIn = False
+    """
     def __init__(self, name, password, phoneNumber, email, age):
         self.name = name 
         self.password = password 
@@ -7,7 +24,9 @@ class User:
         self.email = email
         self.age = age
         self.signedIn = False
-      
+        # TODO: INITIALIZE CART OBJECT BASED ON CART CLASS
+        self.cart = Cart()
+
 
     def signIn(self, email, password):
         self.signedIn = (self.email == email) & (self.password == password)
@@ -29,3 +48,6 @@ class User:
     
     def getSignedIn(self):
         return self.signedIn
+    
+    def getCart(self):
+        return self.cart
